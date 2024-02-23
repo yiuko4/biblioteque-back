1e9a90962de1e93d7d5450c187902d8241b1e01f
purty12 <91609727+purty12@users.noreply.github.com>
2024-02-23 10:59:37 +0100

    test

---
+++ Connect_To_BDD.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+// Configuration de la connexion à la base de données
+const db = mysql.createConnection({
+    host: 'mysql-bibliotheque-bdd.alwaysdata.net',
+    user: '349194',
+    password: '6A(?M{r#;~45hhZJ',
+    database: 'bibliotheque-bdd_1'
+});
+
+// Connexion à la base de données
+db.connect((err) => {
+    if (err) {
+        console.error('Erreur de connexion à la base de données :', err);
+    } else {
+        console.log('Connexion à la base de données établie');
+    }
+});
+
+module.exports = db;
